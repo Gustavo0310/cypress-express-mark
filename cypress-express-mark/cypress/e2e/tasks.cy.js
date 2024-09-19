b5b0263e0b804dee66f2416004ef1a374f49b8b7
Gustavo0310 <gustavolmg2@gmail.com>
2024-09-18 21:34:45 -0300

    Continuação do curso e integração do Allure Report no projeto

--- cypress-express-mark/cypress/e2e/tasks.cy.js
+++ cypress-express-mark/cypress/e2e/tasks.cy.js
@@ -2,13 +2,84 @@
 
 describe('Tarefas', ()=>{
 
-    it('Deve cadastrar uma nova tarefa', ()=>{
-        cy.visit('http://localhost:3000/')
+    let testData;
 
-        /// AULA 12: APRENDENDO COMO MANIPULAR "INPUT" DO TIPO "TEXT"
-        cy.get('input[placeholder="Add a new Task"]') 
-            .type('Ler um livro de Node.JS')
+    before(() => {
+        cy.fixture('tasks').then(t => {
+            testData = t
+        }) 
+    })
 
-            cy.contains('button', "Create").click()
+    context('cadastro', () => {
+        it('Deve cadastrar uma nova tarefa', ()=>{
+
+            const taskName = 'Ler um livro de Node.JS'
+    
+            cy.removeTaskByName(taskName)
+            cy.createTask(taskName)
+    
+            cy.contains('main div p', taskName)
+                .should('be.visible')
+        })
+    
+        it('Não deve permitir tarefa duplicada', () => {
+            
+            const task = testData.dup
+                
+            cy.removeTaskByName(task.name)        
+            cy.postTask(task)
+            cy.createTask(task.name)
+    
+            cy.get('.swal2-html-container')
+                .should('be.visible')
+                .should('have.text', 'Task already exists!')
+        })
+    
+        it('campo obrigatório', () =>{
+            cy.createTask()
+            cy.isRequired('This is a required field')
+        })
+    })
+    context('atualização', () => {
+        it('deve concluir uma tarefa', () => {
+            const task= {
+                name: 'TESTE', 
+                is_done: false 
+            }
+
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+
+            cy.visit('/')
+
+            cy.contains('p', task.name)
+                .parent()
+                .find('button[class*=ItemToggle]')
+                .click()
+
+            cy.contains('p', task.name)
+                .should('have.css', 'text-decoration-line', 'line-through')
+        })
+    })
+    context('exclusão', () => {
+        it('deve remover uma tarefa', () => {
+            const task= {
+                name: 'Estudar JavaScript', 
+                is_done: false 
+            }
+
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+
+            cy.visit('/')
+
+            cy.contains('p', task.name)
+                .parent()
+                .find('button[class*=ItemDelete]')
+                .click()
+
+            cy.contains('p', task.name)
+                .should('not.exist')
+        })
     })
 })
